41a1dfe2e0d6e43317afff188d88963144a384af
mschinta <40994537+mschinta@users.noreply.github.com>
2025-10-21 22:52:34 -0500

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,52 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
-const usersMockdata = () => {
-  return new Promise((resolve,reject) => {
-    setTimeout(() => {
-      resolve([{
-      customerName: 'Madhava',
-      purchaseDate: 'Jul',
-      amount: 140,
-    },
-    {
-      customerName: 'Madhava',
-      purchaseDate: 'Jul',
-      amount: 160,
-    },
-    {
-      customerName: 'Madhava',
-      purchaseDate: 'Aug',
-      amount: 120.9,
-    },
-    {
-      customerName: 'Srini',
-      purchaseDate: 'Aug',
-      amount: 100,
-    },
-     {
-      customerName: 'Srini',
-      purchaseDate: 'Sep',
-      amount: 90,
-    },
-    {
-      customerName: 'kumar',
-      purchaseDate: 'Aug',
-      amount: 60,
-    },
-    {
-      customerName: 'kumar',
-      purchaseDate: 'Sep',
-      amount: 50,
-    },
-    {
-      customerName: 'vish',
-      purchaseDate: 'Sep',
-      amount: 50,
-    }])
-    },1000)
-    
-  })
-}
+import {usersMockdata} from './mockData'
 
 function App() {
  const [customerRewards,setCustomerRewards] = useState({});
